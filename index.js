327b40bd3940181325e10b76f56f697c18c2c47f
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-07 19:46:25 +0300

    darslar bajarildi

---
+++ index.js
@@ -0,0 +1,26 @@
+//Birinhi masala
+//let firstNumber = +prompt("birinchi sonni kiriting");
+//let SecondNumber = +prompt("ikkinchi sonni kiriting");
+//let third_number = +prompt("uchinch sonni kiriting");
+//let res = (firstNumber+SecondNumber+third_number);
+//alert(res)
+
+
+//Ikkinchi masala
+//let a = +prompt("birinchi sonni kiriting");
+//let b = +prompt("ikkinchi sonni kiriting");
+//let result = (a*a+b*b);
+//alert(result);
+
+
+//uchinchi masala
+//let a = +prompt("bo'yini kiriting");
+//let b = +prompt("enini kiriting");
+//let res = (a*2+b*2);
+//alert(res);
+
+
+//beshinchi masala
+let d = +prompt("Doira diametrini kiriting");
+let res = ((d/2)^2*3.14)
+alert(res)
